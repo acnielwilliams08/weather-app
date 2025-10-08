a39c1b19a03f649e75c7544b98f87f82bbb35486
Buty06 <acnielwilliams08@gmail.com>
2025-10-07 21:25:47 -0400

    Se carga la data de las regiones correctamente, ahora metere ese codigo de fetch dentro de un custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { SearchInpit } from "./Components/SearchInpit";
+import { fetchApi } from "./helpers/fetchApi";
 
 export const App = () => {
-  const [region, setRegion] = useState("");
+  const [region, setRegion] = useState("Cuba");
+  const [isLoading, setisLoading] = useState(true);
+  const [data, setdata] = useState({});
 
   const onAddRegion = (region: string) => {
     setRegion(region);
   };
+
+  useEffect(() => {
+    if (region.length <= 0) return;
+
+    fetchApi(region).then((data) => setdata(data));
+
+    setisLoading(false);
+  }, [region]);
+
+  console.log(data);
 
   return (
     <>
       <h1>Weather App</h1>
 
       <SearchInpit onAddRegion={onAddRegion}></SearchInpit>
+
+      {isLoading && <h2>Cargando...</h2>}
     </>
   );
 };
